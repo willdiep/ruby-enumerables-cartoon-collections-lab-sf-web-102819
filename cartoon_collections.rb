4c266f321be088eeddce97d1e1b88b6bc2491e2e
Will Diep <widiep@gmail.com>
2019-09-27 12:21:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,15 @@ def summon_captain_planet(arr)
 	end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+	arr.map do |word|
+		if word.length > 4
+			return true
+		end
+	end
+	return false
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
